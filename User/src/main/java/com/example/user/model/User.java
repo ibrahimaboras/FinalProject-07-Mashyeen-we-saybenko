413b5bbb982a47ca70c425e1b91645f218a8e45c
Ibrahim <ibrahimaboras2@gmail.com>
2025-05-17 02:01:47 +0300

    fixed a slight mistype

--- User/src/main/java/com/example/user/model/User.java
+++ User/src/main/java/com/example/user/model/User.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.Long)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
     private String fullName;
